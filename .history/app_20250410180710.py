6f8cda19f08a8f2daea61c805bae9ef3d0b2f5fc
Deyvyd Moura <deyvyd.dev@gmail.com>
2025-04-11 09:59:39 -0300

    correção da rota /dev pra documentações de desenvolvimento

--- .history/app_20250410180710.py
+++ .history/app_20250410180710.py
@@ -1741,10 +1741,6 @@ def gerar_pdf_do_docx(caminho_arquivo):
 def index():
     return render_template('index.html')
 
-@app.route('/dev')
-def dev_index():
-    return render_template('index-dev.html')
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
