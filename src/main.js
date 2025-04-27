e66992278ddbf6976ddb0b416499403cee35eb60
Deyvyd Moura <deyvyd.dev@gmail.com>
2025-04-27 18:42:26 -0300

    ajuste nos estilos das tabelas

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+
+// Importe seus componentes aqui
+import App from './components/App.vue'
+
+const app = createApp({
+  // Configurações do seu aplicativo
+})
+
+// Registre componentes globais aqui
+// app.component('component-name', Component)
+
+app.mount('#app') 
