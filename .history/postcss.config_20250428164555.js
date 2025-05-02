44d5a6e48139af2c826fba90469f0bda7937f4a5
Deyvyd Moura <53580499+deyvyd@users.noreply.github.com>
2025-05-02 08:44:49 -0300

    Delete .history directory

--- .history/postcss.config_20250428164555.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {}, // Isso deve vir primeiro
-    "tailwindcss/nesting": {}, // Adicione o plugin de nesting
-    tailwindcss: {}, // Depois o tailwind
-    autoprefixer: {}, // Por último o autoprefixer
-  },
-};
