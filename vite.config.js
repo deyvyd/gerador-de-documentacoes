26076b047df823afbe12e197ef4e84d6d99255c3
Deyvyd Moura <deyvyd.dev@gmail.com>
2026-06-12 10:42:23 -0300

    fix: set base path /aiyra/docgen/ for Vercel proxy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
   plugins: [vue()],
+  base: "/aiyra/docgen/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
